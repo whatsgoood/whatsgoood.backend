454db88565c88b6fc92852ff9f0ee3c089d379d4
Robert Kritzinger <kritz.rob@gmail.com>
2020-03-26 19:37:27 +0200

    update macScraper.py

--- scraper/macScraper.py
+++ scraper/macScraper.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
 import time
 import pymongo
 from os import environ
@@ -8,11 +14,11 @@ options = webdriver.ChromeOptions()
 options.add_argument("user-data-dir=C:\\Users\\kritz\\AppData\\Local\\Google\\Chrome\\Selenium Data")
 
 browser = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
-browser.implicitly_wait(2)
+wait = WebDriverWait(browser, 10)
 
 macWindSite = 'https://mac-wind.appspot.com/'
 
-mongoUrl = f"mongodb+srv://{environ.get('MONGO_USER')}:{environ.get('MONGO_KEY')}@sandboxcluster-xg5gf.mongodb.net/test?retryWrites=true&w=majority"
+mongoUrl = environ.get('WHATSGOOD_CONSTR')
 
 client = pymongo.MongoClient(mongoUrl)
 db = client['plagiarismDB']
@@ -20,6 +26,8 @@ macWindCol = db['macWindCollection']
 
 while True:
     browser.get(macWindSite)
+    wait.until(ec.visibility_of_element_located((By.XPATH, "(//tr)[2]/td")))
+    
     topRow = browser.find_elements_by_xpath("(//tr)[2]/td")
     avg = float(topRow[0].text)
     low = float(topRow[1].text)
