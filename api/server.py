25c3c1b06d9536c76b743d11f41332c85515e512
Dylan Graham <dgraham365@gmail.com>
2020-04-05 10:39:27 +0200

    FEAT: getSportRatingList
    - Added endpoint for the sport list that returns stubbed json data
    - Added some TODOs
    
    FIXES: #3

--- api/server.py
+++ api/server.py
@@ -1,9 +1,35 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'ekse'
+@app.route('/api/sport-rating-list')
+def sportRatingList():
+    # TODO: 
+    #
+    # 1) Connect to MongoDB
+    #
+    # 2) Build rating for each sport
+    #    using sport-specific weights
+    #
+    # Example: 
+    #     Kiting would have wind weighted high
+    #     in comparision to the temperature
+
+    return {
+        "sportRating": [
+            {
+                "name": "kiting",
+                "rating": 6.2 
+            },
+            {
+                "name": "surfing",
+                "rating": 9.4 
+            },
+            {
+                "name": "climbing",
+                "rating": 2.0 
+            }
+        ]
+    }
 
 if __name__ == '__main__':
     app.run()
