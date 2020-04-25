f365eeb2e0154abf81ef4b23257db68c17aa1632
Robert Kritzinger <kritz.rob@gmail.com>
2020-04-25 11:43:33 +0200

    small cleanup

--- api/app/main/sportEvaluator/error/incompleteWeightError.py
+++ api/app/main/sportEvaluator/error/incompleteWeightError.py
@@ -2,9 +2,6 @@ class incompleteWeightError(Exception):
 
     message = "Weights did not add up to 1"
 
-    def __init__(self, sport = None):
-        if sport != None:
+    def __init__(self, sport=None):
+        if sport is not None:
             message = f"Weights for {sport} did not add up to 1"
-
-
-    
